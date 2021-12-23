fe152d81746efa2d6a22c5af99d76adba332484e
Jerryuhoo <jerryuhoo@gmail.com>
2021-12-23 16:28:46 +0800

    add tqdm to change sr

--- tools/change_sr.py
+++ tools/change_sr.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import librosa
 import soundfile as sf
+import tqdm
 
 def main():
     parser = argparse.ArgumentParser(
@@ -20,15 +21,16 @@ def main():
 
     g = os.walk(args.path)
     sample_rate = int(args.sr)
+    
     for path, dir_list, file_list in g:
-        for file in file_list:
+        for file in tqdm.tqdm(file_list, total=len(file_list)):
             if file.endswith('.wav') or file.endswith('.mp3'):
                 file_path = os.path.join(args.path, file)
-                print("resampling: ", file_path)
+                # print("resampling: ", file_path)
                 y, sr = librosa.load(file_path, sr=sample_rate)
                 if file.endswith('.mp3'):
                     file_path = os.path.join(args.path, os.path.splitext(file)[0] + ".wav")
-                sf.write(file_path, y, sample_rate, subtype='PCM_24')
+                sf.write(file_path, y, sample_rate, subtype='PCM_16')
     print("change sample rate to ", sample_rate, " , Done.")
 
 if __name__ == '__main__':
