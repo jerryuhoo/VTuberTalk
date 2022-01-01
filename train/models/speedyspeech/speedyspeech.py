4048f33e96b6e246d3bdcc5e58078f6bf2bd44f5
jerryuhoo <Jerryuhoo@gmail.com>
2022-01-01 13:57:24 +0800

    remove useless params in speedyspeech.py

--- train/models/speedyspeech/speedyspeech.py
+++ train/models/speedyspeech/speedyspeech.py
@@ -96,7 +96,7 @@ class TextEmbedding(nn.Layer):
 
 class SpeedySpeechEncoder(nn.Layer):
     def __init__(self, vocab_size, tone_size, hidden_size, kernel_size,
-                 dilations, spk_num):
+                 dilations, spk_num=None):
         super().__init__()
         self.embedding = TextEmbedding(
             vocab_size,
@@ -104,10 +104,13 @@ class SpeedySpeechEncoder(nn.Layer):
             tone_size,
             padding_idx=0,
             tone_padding_idx=0)
-        self.spk_emb = nn.Embedding(
-            num_embeddings=spk_num,
-            embedding_dim=hidden_size,
-            padding_idx=0)
+        
+        if self.spk_num:
+            self.spk_emb = nn.Embedding(
+                num_embeddings=spk_num,
+                embedding_dim=hidden_size,
+                padding_idx=0)
+                
         self.prenet = nn.Sequential(
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(), )
@@ -177,10 +180,7 @@ class SpeedySpeech(nn.Layer):
             decoder_kernel_size,
             decoder_dilations,
             tone_size=None, 
-            spk_num: int=None,
-            spk_embed_dim: int=None,
-            spk_embed_integration_type: str="add",
-            ):
+            spk_num=None):
         super().__init__()
         encoder = SpeedySpeechEncoder(vocab_size, tone_size,
                                       encoder_hidden_size, encoder_kernel_size,
@@ -192,26 +192,6 @@ class SpeedySpeech(nn.Layer):
         self.encoder = encoder
         self.duration_predictor = duration_predictor
         self.decoder = decoder
-        self.spk_embed_dim = spk_embed_dim
-        # use idx 0 as padding idx
-        self.padding_idx = 0
-
-        # if self.spk_embed_dim is not None:
-        #     self.spk_embed_integration_type = spk_embed_integration_type
-        # if spk_num and self.spk_embed_dim:
-        #     self.spk_embedding_table = nn.Embedding(
-        #         num_embeddings=spk_num,
-        #         embedding_dim=self.spk_embed_dim,
-        #         padding_idx=self.padding_idx)
-        # self.encoder_hidden_size = encoder_hidden_size
-        # # define additional projection for speaker embedding
-        
-        # if self.spk_embed_dim is not None:
-        #     if self.spk_embed_integration_type == "add":
-        #         self.spk_projection = nn.Linear(self.spk_embed_dim, self.encoder_hidden_size)
-        #     else:
-        #         self.spk_projection = nn.Linear(self.encoder_hidden_size + self.spk_embed_dim, self.encoder_hidden_size)
-
 
     def forward(self, text, tones, durations, spk_id: paddle.Tensor=None):
         # input of embedding must be int64
@@ -221,12 +201,6 @@ class SpeedySpeech(nn.Layer):
             spk_id = paddle.cast(spk_id, 'int64')
         durations = paddle.cast(durations, 'int64')
         encodings = self.encoder(text, tones, spk_id)
-        # (B, T)
-
-        # if self.spk_embed_dim is not None:
-        #     if spk_id is not None:
-        #         spk_emb = self.spk_embedding_table(spk_id)
-        #         encodings = self._integrate_with_spk_embed(encodings, spk_emb)
 
         pred_durations = self.duration_predictor(encodings.detach())
 
@@ -241,7 +215,7 @@ class SpeedySpeech(nn.Layer):
         decoded = self.decoder(encodings)
         return decoded, pred_durations
 
-    def inference(self, text, tones=None, spk_id=None,):
+    def inference(self, text, tones=None, spk_id=None):
         # text: [T]
         # tones: [T]
         # input of embedding must be int64
@@ -252,10 +226,6 @@ class SpeedySpeech(nn.Layer):
             tones = tones.unsqueeze(0)
 
         encodings = self.encoder(text, tones, spk_id)
-        # if self.spk_embed_dim is not None:
-        #     if spk_id is not None:
-        #         spk_emb = self.spk_embedding_table(spk_id)
-        #         encodings = self._integrate_with_spk_embed(encodings, spk_emb)
 
         pred_durations = self.duration_predictor(encodings)  # (1, T)
         durations_to_expand = paddle.round(pred_durations.exp())
@@ -281,35 +251,6 @@ class SpeedySpeech(nn.Layer):
         encodings += sinusoid_position_encoding(t_dec, feature_size)
         decoded = self.decoder(encodings)
         return decoded[0]
-
-    def _integrate_with_spk_embed(self, hs, spk_emb):
-        """Integrate speaker embedding with hidden states.
-
-        Parameters
-        ----------
-        hs : Tensor
-            Batch of hidden state sequences (B, Tmax, adim).
-        spk_emb : Tensor
-            Batch of speaker embeddings (B, spk_embed_dim).
-
-        Returns
-        ----------
-        Tensor
-            Batch of integrated hidden state sequences (B, Tmax, adim)
-        """
-        if self.spk_embed_integration_type == "add":
-            # apply projection and then add to hidden states
-            spk_emb = self.spk_projection(F.normalize(spk_emb))
-            hs = hs + spk_emb.unsqueeze(1)
-        elif self.spk_embed_integration_type == "concat":
-            # concat hidden states with spk embeds and then apply projection
-            spk_emb = F.normalize(spk_emb).unsqueeze(1).expand(
-                shape=[-1, hs.shape[1], -1])
-            hs = self.spk_projection(paddle.concat([hs, spk_emb], axis=-1))
-        else:
-            raise NotImplementedError("support only add or concat.")
-
-        return hs
 
 class SpeedySpeechInference(nn.Layer):
     def __init__(self, normalizer, speedyspeech_model):
