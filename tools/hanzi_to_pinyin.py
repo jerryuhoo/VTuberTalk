959704cd9844e6c6e6666034273e54fdd9c3f76f
Jerryuhoo <jerryuhoo@gmail.com>
2022-01-10 18:12:19 +0800

    Add ignore punctuation to hanzi 2 pinyin

--- tools/hanzi_to_pinyin.py
+++ tools/hanzi_to_pinyin.py
@@ -1,6 +1,7 @@
 from pypinyin import pinyin, Style
 import os
 import argparse
+import re
 
 def get_pinyin_from_text(text):
     pinyins = [
@@ -8,7 +9,7 @@ def get_pinyin_from_text(text):
     ]
     return pinyins
 
-def process(path):
+def process(path, ignore_punc):
     is_dir = os.path.isdir(path)
     if is_dir: 
         path_list=os.listdir(path)
@@ -19,7 +20,7 @@ def process(path):
     for filename in path_list:
         root_folder = os.path.join(path, filename)
         if os.path.isdir(root_folder):
-            process(root_folder)
+            process(root_folder, ignore_punc)
         filename_suffix = os.path.splitext(filename)[1]
         input_file_path = os.path.join(path, filename)
         output_file_path = os.path.join(path, os.path.splitext(filename)[0] + ".lab")
@@ -34,6 +35,11 @@ def process(path):
                     "w",
                 ) as f1:
                     for pinyin in pinyins:
+                        if ignore_punc:
+                            rule = re.compile(u"[^a-zA-Z0-9]")
+                            pinyin = rule.sub('', pinyin)
+                            if pinyin == '':
+                                continue
                         f1.write("%s " % pinyin)
             f1.close()
             # print("write to " + output_file_path)
@@ -41,15 +47,19 @@ def process(path):
             # print("file ", filename, " format not supported!")
             continue
 
+def str2bool(str):
+    return True if str.lower() == 'true' else False
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--ignore_punc", type=str2bool, default=True)
     args = parser.parse_args()
 
     is_exist = os.path.exists(args.path)
     if not is_exist:
         print("path not existed!")
     else:
-        process(args.path)
+        process(args.path, args.ignore_punc)
 
     print("done!")
