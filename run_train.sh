176628f1318eaba8545972e12897f0586baa6525
Jerryuhoo <jerryuhoo@gmail.com>
2022-01-21 13:54:37 +0800

    Auto copy files to model after training

--- run_train.sh
+++ run_train.sh
@@ -2,11 +2,100 @@ set -e
 
 stage=0
 stop_stage=100
-model_name=vae_fastspeech2_azi_nanami
-fastspeech2=True
+model_name=speedyspeech_azi_nanami_1_15 #speedyspeech_azi_nanami_1_9 #gst_fastspeech2_azi_nanami_2022_1_9 #gst_fastspeech2_4people_new
+fastspeech2=False
 multiple=True
 use_gst=False
-use_vae=True
+use_vae=False
+
+if [ ${fastspeech2} == True ] && [ ${multiple} == False ]; then
+    echo "model: fastspeech2, single"
+
+    if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+        echo "duration"
+        python tools/gen_duration_from_textgrid.py \
+            --inputdir=data/TextGrid \
+            --output=data/durations.txt \
+            --config=train/conf/fastspeech2/default_single.yaml || exit -1
+    fi
+
+    if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+        echo "preprocess"
+        python train/exps/fastspeech2/preprocess.py \
+            --dataset=other \
+            --rootdir=data/ \
+            --dumpdir=dump \
+            --dur-file=data/durations.txt \
+            --config=train/conf/fastspeech2/default_single.yaml \
+            --num-cpu=16 \
+            --cut-sil=True || exit -1
+    fi
+
+    if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+        echo "compute statistics"
+        python tools/compute_statistics.py \
+            --metadata=dump/train/raw/metadata.jsonl \
+            --field-name="speech" || exit -1
+
+        python tools/compute_statistics.py \
+            --metadata=dump/train/raw/metadata.jsonl \
+            --field-name="pitch" || exit -1
+
+        python tools/compute_statistics.py \
+            --metadata=dump/train/raw/metadata.jsonl \
+            --field-name="energy" || exit -1
+    fi
+
+    if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+        echo "normalize"
+        python train/exps/fastspeech2/normalize.py \
+            --metadata=dump/train/raw/metadata.jsonl \
+            --dumpdir=dump/train/norm \
+            --speech-stats=dump/train/speech_stats.npy \
+            --pitch-stats=dump/train/pitch_stats.npy \
+            --energy-stats=dump/train/energy_stats.npy \
+            --phones-dict=dump/phone_id_map.txt || exit -1
+
+        python train/exps/fastspeech2/normalize.py \
+            --metadata=dump/dev/raw/metadata.jsonl \
+            --dumpdir=dump/dev/norm \
+            --speech-stats=dump/train/speech_stats.npy \
+            --pitch-stats=dump/train/pitch_stats.npy \
+            --energy-stats=dump/train/energy_stats.npy \
+            --phones-dict=dump/phone_id_map.txt || exit -1
+
+        python train/exps/fastspeech2/normalize.py \
+            --metadata=dump/test/raw/metadata.jsonl \
+            --dumpdir=dump/test/norm \
+            --speech-stats=dump/train/speech_stats.npy \
+            --pitch-stats=dump/train/pitch_stats.npy \
+            --energy-stats=dump/train/energy_stats.npy \
+            --phones-dict=dump/phone_id_map.txt || exit -1
+    fi
+
+    if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4]; then
+        echo "train"
+        python train/exps/fastspeech2/train.py \
+            --train-metadata=dump/train/norm/metadata.jsonl \
+            --dev-metadata=dump/dev/norm/metadata.jsonl \
+            --config=train/conf/fastspeech2/default_multi.yaml \
+            --output-dir=exp/$model_name \
+            --ngpu=1 \
+            --phones-dict=dump/phone_id_map.txt \
+            --use_gst=$use_gst \
+            --use_vae=$use_vae || exit -1
+    fi
+
+    if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+        echo "copy files to model"
+        cp dump/phone_id_map.txt exp/$model_name/phone_id_map.txt
+        cp dump/speaker_id_map.txt exp/$model_name/speaker_id_map.txt
+        cp dump/train/energy_stats.npy exp/$model_name/energy_stats.npy
+        cp dump/train/pitch_stats.npy exp/$model_name/pitch_stats.npy
+        cp dump/train/speech_stats.npy exp/$model_name/speech_stats.npy
+    fi
+fi
+
 
 if [ ${fastspeech2} == True ] && [ ${multiple} == True ]; then
     echo "model: fastspeech2, multiple"
@@ -89,86 +178,14 @@ if [ ${fastspeech2} == True ] && [ ${multiple} == True ]; then
             --use_gst=$use_gst \
             --use_vae=$use_vae || exit -1
     fi
-fi
 
-
-
-if [ ${fastspeech2} == True ] && [ ${multiple} == False ]; then
-    echo "model: fastspeech2, single"
-
-    if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-        echo "duration"
-        python tools/gen_duration_from_textgrid.py \
-            --inputdir=data/TextGrid \
-            --output=data/durations.txt \
-            --config=train/conf/fastspeech2/default_single.yaml || exit -1
-    fi
-
-    if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-        echo "preprocess"
-        python train/exps/fastspeech2/preprocess.py \
-            --dataset=other \
-            --rootdir=data/ \
-            --dumpdir=dump \
-            --dur-file=data/durations.txt \
-            --config=train/conf/fastspeech2/default_single.yaml \
-            --num-cpu=16 \
-            --cut-sil=True || exit -1
-    fi
-
-    if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-        echo "compute statistics"
-        python tools/compute_statistics.py \
-            --metadata=dump/train/raw/metadata.jsonl \
-            --field-name="speech" || exit -1
-
-        python tools/compute_statistics.py \
-            --metadata=dump/train/raw/metadata.jsonl \
-            --field-name="pitch" || exit -1
-
-        python tools/compute_statistics.py \
-            --metadata=dump/train/raw/metadata.jsonl \
-            --field-name="energy" || exit -1
-    fi
-
-    if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-        echo "normalize"
-        python train/exps/fastspeech2/normalize.py \
-            --metadata=dump/train/raw/metadata.jsonl \
-            --dumpdir=dump/train/norm \
-            --speech-stats=dump/train/speech_stats.npy \
-            --pitch-stats=dump/train/pitch_stats.npy \
-            --energy-stats=dump/train/energy_stats.npy \
-            --phones-dict=dump/phone_id_map.txt || exit -1
-
-        python train/exps/fastspeech2/normalize.py \
-            --metadata=dump/dev/raw/metadata.jsonl \
-            --dumpdir=dump/dev/norm \
-            --speech-stats=dump/train/speech_stats.npy \
-            --pitch-stats=dump/train/pitch_stats.npy \
-            --energy-stats=dump/train/energy_stats.npy \
-            --phones-dict=dump/phone_id_map.txt || exit -1
-
-        python train/exps/fastspeech2/normalize.py \
-            --metadata=dump/test/raw/metadata.jsonl \
-            --dumpdir=dump/test/norm \
-            --speech-stats=dump/train/speech_stats.npy \
-            --pitch-stats=dump/train/pitch_stats.npy \
-            --energy-stats=dump/train/energy_stats.npy \
-            --phones-dict=dump/phone_id_map.txt || exit -1
-    fi
-
-    if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4]; then
-        echo "train"
-        python train/exps/fastspeech2/train.py \
-            --train-metadata=dump/train/norm/metadata.jsonl \
-            --dev-metadata=dump/dev/norm/metadata.jsonl \
-            --config=train/conf/fastspeech2/default_multi.yaml \
-            --output-dir=exp/$model_name \
-            --ngpu=1 \
-            --phones-dict=dump/phone_id_map.txt \
-            --use_gst=$use_gst \
-            --use_vae=$use_vae || exit -1
+    if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+        echo "copy files to model"
+        cp dump/phone_id_map.txt exp/$model_name/phone_id_map.txt
+        cp dump/speaker_id_map.txt exp/$model_name/speaker_id_map.txt
+        cp dump/train/energy_stats.npy exp/$model_name/energy_stats.npy
+        cp dump/train/pitch_stats.npy exp/$model_name/pitch_stats.npy
+        cp dump/train/speech_stats.npy exp/$model_name/speech_stats.npy
     fi
 fi
 
@@ -247,6 +264,14 @@ if [ ${fastspeech2} == False ] && [ ${multiple} == False ]; then
             --tones-dict=dump/tone_id_map.txt \
             --use-relative-path=True || exit -1
     fi
+
+    if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+        echo "copy files to model"
+        cp dump/phone_id_map.txt exp/$model_name/phone_id_map.txt
+        cp dump/speaker_id_map.txt exp/$model_name/speaker_id_map.txt
+        cp dump/tone_id_map.txt exp/$model_name/tone_id_map.txt
+        cp dump/train/feats_stats.npy exp/$model_name/feats_stats.npy
+    fi
 fi
 
 
@@ -324,5 +349,13 @@ if [ ${fastspeech2} == False ] && [ ${multiple} == True ]; then
             --tones-dict=dump/tone_id_map.txt \
             --speaker-dict=dump/speaker_id_map.txt \
             --use-relative-path=True || exit -1
+    fi
+
+    if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+        echo "copy files to model"
+        cp dump/phone_id_map.txt exp/$model_name/phone_id_map.txt
+        cp dump/speaker_id_map.txt exp/$model_name/speaker_id_map.txt
+        cp dump/tone_id_map.txt exp/$model_name/tone_id_map.txt
+        cp dump/train/feats_stats.npy exp/$model_name/feats_stats.npy
     fi
 fi
